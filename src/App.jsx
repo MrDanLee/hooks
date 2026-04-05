c2f5d16ff5cc5a84379d0dcb2c0a111a0c07e9d7
MrDanLee <daniel23lozano@gmail.com>
2026-04-05 11:39:08 +0200

    animación loading y demás css por Data

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,38 @@
-import useFetchCharacters from './hooks/useFetchCharacters';
 import './App.css';
+import useFetchCharacters from './hooks/useFetchCharacters';
+import Character from './components/Character';
+
 
 function App() {
-  const urlPokemon = 'https://pokeapi.co/api/v2/pokemon/10/';
+  const urlPokemon = `https://pokeapi.co/api/v2/pokemon/2`;
   const urlRick = 'https://rickandmortyapi.com/api/character/10';
 
-  const pokemon = useFetchCharacters(urlPokemon)
+  const {data: pokemon, loading: loadingPokemon, error: errorPokemon} = useFetchCharacters(urlPokemon)
+  const {data: rick, loading: loadingRick, error: errorRick} = useFetchCharacters(urlRick);
   // const {data:  pokemon} = useFetchCharacters 
   // Si quiero retornar un objeto, hago un destructuring. 
   // En caso de retornar solo un elemento, puedo omitirlo. 
 
-  const rick = useFetchCharacters(urlRick)
+  // const {data: rick, loading:loadingRick, error: errorRick} = useFetchCharacters(urlRick)
+
+
 
   return (
     <>
-    <section>
-    {
-      pokemon 
-      ? <>
-        <div>{pokemon?.name}</div>
-        <div><img src={pokemon?.sprites?.front_default} alt={pokemon?.name} /></div>
-      </>
-      : "No hay pokemon"
-    }
-    </section>
-    <section>
-    { 
-      rick
-      ? <>
-        <div>{rick?.name}</div>
-        <div><img src={rick?.image} alt={rick?.name} /></div>
-      
-      </>
-      : "No hay personaje"
-    }
-    </section>
+    <Character
+      character={pokemon}
+      loading={loadingPokemon}
+      error={errorPokemon}
+      name={pokemon?.name}
+      img={pokemon?.sprites?.front_default}
+    />
+    <Character
+      character={rick}
+      loading={loadingRick}
+      error={errorRick}
+      name={rick?.name}
+      img={rick?.image}
+    />
     </>
   );
 }
--- src/hooks/useFetchCharacters.js
+++ src/hooks/useFetchCharacters.js
@@ -1,22 +1,36 @@
 import { useEffect, useState } from 'react';
 const useFetchCharacters = (url) => {
   const [data, setData] = useState(null)
+  const [error, setError] = useState(null)
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
+    setLoading(true)
+    setError(null)
+    setData(null)
+
     async function fetchCharacter() {
       try {
         const response = await fetch(url)
+        if(!response.ok) {
+          throw new Error(`Error: ${response.status}`)
+        }
         const result = await response.json()
         setData(result)
         console.log(data)
       } catch (err) {
-        console.error(err)
+        console.error(`Fetch Error: ${err}`)
+        setError(err.message || "Ha ocurrido algo inesperado")
+      } finally {
+        setLoading(false)
       }
     }
-    fetchCharacter()
+    if(url) {
+      fetchCharacter()
+    } 
   }, [url])
 
-  return data
+  return {data, error, loading}
 }
 
 export default useFetchCharacters;
